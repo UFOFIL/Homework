7dfb7000ec6a843478a958fa31593b09379060e9
UFOFIL <redfield7771@gmail.com>
2022-11-16 13:55:06 +0200

    homework

--- src/Ex5.java
+++ src/Ex5.java
@@ -5,20 +5,27 @@ public class Ex5 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your number");
         int number = scanner.nextInt();
-        int num1=0;
-        int num2=1;
+        int num1 = 0;
+        int num2 = 1;
         int num3;
-        for (int i=1;i<=nu;i++){
-            //прибавляем
-            num3=num1+num2;
-            System.out.println(num3);
-            if(number==num3){
-                System.out.println("Yes it is Fibonachi");
+        for (int i = 1; i <= number; i++) {
+            num3 = num1 + num2;
+            if (num3==number){
+                System.out.println("Fibonachi");
+                boolean isFibo=true;
             }
-            else {
-                System.out.println("Not Fibonachi");
-            }
-            //обновляем
-            num1=num2;
-            num2=num3;
+            num1 = num2;
+            num2 = num3;
+
         }
+
+    }
+
+}
+
+
+
+
+
+
+
